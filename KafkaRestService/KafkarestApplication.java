2be03af1ae7a0d29e21df17f67e3677a1edb5ded
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-09 22:33:29 +0530

    Create KafkarestApplication.java

---
+++ KafkaRestService/KafkarestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservice.kafkarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkarestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkarestApplication.class, args);
+    }
+
+}
