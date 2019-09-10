074e0fa6504372e8e8bcbe6c0e1482bc536f2ec1
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-10 21:59:42 +0530

    Update Consumer.java

--- KafkaRestService/Consumer.java
+++ KafkaRestService/Consumer.java
@@ -1,5 +1,6 @@
 package com.example.restservice.kafkarest;
 
+import org.apache.kafka.common.protocol.types.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,9 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class Consumer {
     private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+    String consumedMessage;
 
     @KafkaListener(topics = "sample", groupId = "group_id")
     public void consume(String message){
         logger.info(String.format("$$ -> Consumed Message -> %s",message));
+        consumedMessage = message;
+    }
+
+    public String getMessageConsumed(){
+        return consumedMessage;
     }
 }
