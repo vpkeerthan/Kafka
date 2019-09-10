74cc0fc789894d01ccb7d1d57519e65a4ab2e70d
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-10 21:58:48 +0530

    Update KafkaController.java

--- KafkaRestService/KafkaController.java
+++ KafkaRestService/KafkaController.java
@@ -1,23 +1,27 @@
 package com.example.restservice.kafkarest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/kafka")
 public class KafkaController {
     private final Producer producer;
+    private final Consumer consumer;
 
     @Autowired
-    public KafkaController(Producer producer){
+    public KafkaController(Producer producer, Consumer consumer){
         this.producer = producer;
+        this.consumer = consumer;
     }
 
     @PostMapping(value = "/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message){
         this.producer.sendMessage(message);
+    }
+
+    @GetMapping(value = "/receive")
+    public String getMessageFromConsumer(){
+        return this.consumer.getMessageConsumed();
     }
 }
